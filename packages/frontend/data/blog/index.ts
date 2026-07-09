759312eb4fb513ad3ed62697bf562d1021d57038
Patrick <deseee@gmail.com>
2026-07-09 15:55:30 -0400

    feat: reconcile scraper subtypes + add shopper-facing subtype filtering (ADR-023 Phase 2)

--- packages/frontend/data/blog/index.ts
+++ packages/frontend/data/blog/index.ts
@@ -24,8 +24,9 @@ import { postK } from './posts/state-of-secondary-sales-in-america-2026';
 import { postL } from './posts/when-estate-sales-and-yard-sales-actually-happen';
 import { postM } from './posts/estate-sale-company-density-us-cities';
 import { postN } from './posts/why-cant-i-just-sell-it-myself';
+import { postO } from './posts/what-sells-collectibles-military-sports-memorabilia';
 
-export const posts: BlogPost[] = [postA, postB, postC, postD, postE, postF, postG, postH, postI, postJ, postK, postL, postM, postN];
+export const posts: BlogPost[] = [postA, postB, postC, postD, postE, postF, postG, postH, postI, postJ, postK, postL, postM, postN, postO];
 
 export function getPostBySlug(slug: string): BlogPost | undefined {
   return posts.find((p) => p.slug === slug);
